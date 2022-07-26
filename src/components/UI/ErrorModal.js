9ba04ede57f473ac2f42c63ccfeb33f70cf350f4
Farshid Karimi <farshidk49@gmail.com>
2022-07-26 23:22:57 +0430

    useState -> useRef

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,28 +1,44 @@
-import React from 'react'
-import Button from './Button'
-import Card from './Card'
-import './errormodal.scss'
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "./Button";
+import Card from "./Card";
+import "./errormodal.scss";
 
 const ErrorModal = (props) => {
+  const Backdrop = (props) => {
+    return <div className="backdrop" onClick={props.onOkay} />;
+  };
+
+  const Modal = (props) => {
+    return (
+      <Card passClass="modal">
+        <header className="header">{props.title}</header>
+
+        <div className="content">{props.message}</div>
+
+        <footer className="actions">
+          <Button onClick={props.onOkay}>Okay</Button>
+        </footer>
+      </Card>
+    );
+  };
+
   return (
     <>
-    <div className='backdrop' onClick={props.onOkay}/>
-    <Card passClass="modal">
-        <header className='header'>
-            {props.title}
-        </header>
-
-        <div className='content'>
-            {props.message}
-        </div>
-
-        <footer className='actions'>
-            <Button onClick={props.onOkay}>Okay</Button>
-        </footer>
-    </Card>
+      {ReactDOM.createPortal(
+        <Backdrop onOkay={props.onOkay} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <Modal
+          title={props.title}
+          message={props.message}
+          onOkay={props.onOkay}
+        />,
+        document.getElementById("modal-root")
+      )}
     </>
-    
-  )
-}
+  );
+};
 
-export default ErrorModal
+export default ErrorModal;
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,72 +1,85 @@
-import React from 'react'
-import { useState } from 'react';
-import Button from '../UI/Button';
-import Card from '../UI/Card';
-import ErrorModal from '../UI/ErrorModal';
-import './adduser.scss'
+import React from "react";
+import { useRef } from "react";
+import { useState } from "react";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import ErrorModal from "../UI/ErrorModal";
+import "./adduser.scss";
 
 const AddUser = (props) => {
 
-  const [enteredUsername, setEnteredUsename] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
   const [error, setError] = useState();
 
-  const addUserHandler = (event) =>{
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
+  const addUserHandler = (event) => {
     event.preventDefault();
 
-    if((enteredUsername.trim().length === 0) || (enteredAge.trim().length === 0)){
-        setError({
-            title: "empty input!",
-            message: "you left one of the age or username empty"
-        })
-        return;
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+
+
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
+      setError({
+        title: "empty input!",
+        message: "you left one of the age or username empty",
+      });
+      return;
     }
 
     //plus for ensure convert string to number
-    if(+enteredAge < 1){
-        setError({
-            title: "invalid age!",
-            message: "enter an age greater than 0"
-        })
-        return;
+    if (+enteredUserAge < 1) {
+      setError({
+        title: "invalid age!",
+        message: "enter an age greater than 0",
+      });
+      return;
     }
 
-    props.onAddUser(enteredUsername, enteredAge)
-    // console.log([enteredAge, enteredUsername])
-    setEnteredAge('')
-    setEnteredUsename('')
-  }
+    props.onAddUser(enteredName, enteredUserAge);
 
-  const onChangeEnteredUsename = (event)=>{
-    setEnteredUsename(event.target.value)
-  }
+    //manipulate DOM Manaul
+    //Don't do this
+    //useRef just use use instead of useState when we have a single read
+    nameInputRef.current.value = ''
+    ageInputRef.current.value = ''
 
-  const onChangeEnteredAge = (event)=>{
-    setEnteredAge(event.target.value)
-  }
+  };
 
-  const modalErrorOkay = ()=>{
+  const modalErrorOkay = () => {
     setError(false);
-  }
+  };
 
   return (
     <>
-    {
-        error &&  <ErrorModal title={error.title} message={error.message} onOkay={modalErrorOkay}/>
-    }
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onOkay={modalErrorOkay}
+        />
+      )}
 
-    <Card passClass="input">
+      <Card passClass="input">
         <form onSubmit={addUserHandler}>
-            <label htmlFor='username'>Username</label>
-            <input id="username" value={enteredUsername} type="text" onChange={onChangeEnteredUsename}></input>
-            <label htmlFor='age'>Age</label>
-            <input id="age" value={enteredAge} type="number" onChange={onChangeEnteredAge}></input>
-            <Button type="submit">Add User</Button>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            ref={nameInputRef}
+          ></input>
+          <label htmlFor="age">Age</label>
+          <input
+            id="age"
+            type="number"
+            ref={ageInputRef}
+          ></input>
+          <Button type="submit">Add User</Button>
         </form>
-    </Card>
+      </Card>
     </>
-    
-  )
-}
+  );
+};
 
-export default AddUser
+export default AddUser;
